ee506d4e53221a92096a0ab04a915ba6068c3b75
turbogorogoso <naddanai_turbo@outlook.com>
2024-10-26 17:06:25 +0700

    print function address

--- main.cpp
+++ main.cpp
@@ -5,35 +5,33 @@ int global_var1 = 1;
 int global_var2 = 2;
 int global_var3 = 3;
 
-// Function to print the address of variables
-void print_addresses(const char* func_name, int* local_var) {
-    std::cout << func_name << " - Local variable address: " << local_var << std::endl;
+// First function to demonstrate stack memory
+void print_addresses(void (*func)(), const char* func_name, int* local_var) {
+    std::cout << func_name << " - Function address: " << (void*)func << "\n";
+    std::cout << "Local variable address: " << local_var << "\n";
     std::cout << "Global variable 1 address: " << &global_var1 << "\n";
     std::cout << "Global variable 2 address: " << &global_var2 << "\n";
     std::cout << "Global variable 3 address: " << &global_var3 << "\n\n";
 }
 
-// First function to demonstrate stack memory
+// Functions to demonstrate
 void function1() {
     int local_var1 = 10;
-    print_addresses("function1", &local_var1);
+    print_addresses(function1, "function1", &local_var1);
 }
 
-// Second function to demonstrate stack memory with another layer of function calls
 void function2() {
     int local_var2 = 20;
-    print_addresses("function2", &local_var2);
+    print_addresses(function2, "function2", &local_var2);
     function1(); // calling function1 within function2
 }
 
-// Third function to demonstrate stack memory with yet another layer of function calls
 void function3() {
     int local_var3 = 30;
-    print_addresses("function3", &local_var3);
+    print_addresses(function3, "function3", &local_var3);
     function2(); // calling function2 within function3
 }
 
-// Function to demonstrate dynamic memory allocation using malloc
 void demonstrate_malloc() {
     // Allocate 3 blocks of memory
     int* heap_var1 = (int*)malloc(sizeof(int));
@@ -62,8 +60,8 @@ int main() {
 
     // Demonstrate dynamic memory allocation
     demonstrate_malloc();
-    int main_var4 = 40;
-    std::cout << "\nMain - Local variable address: " << &main_var4 << "\n";
+    int main_var = 40;
+    std::cout << "\nMain - Local variable address: " << &main_var << "\n";
 
     return 0;
 }
